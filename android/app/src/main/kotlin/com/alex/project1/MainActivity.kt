478c878979b6f58f2c1625de4e4cd6b5eca77378
Eric <jenchieh19@gmail.com>
2023-12-05 21:05:15 +0800

    Eric

---
+++ android/app/src/main/kotlin/com/alex/project1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alex.project1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,120 @@
-/*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * Author: Nelson Chung
- * Creation Date: September 12, 2023
- */
-
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_calendar_week/flutter_calendar_week.dart';
+import 'package:intl/intl.dart';
+import 'page2.dart';
 
-void main() => runApp(MyApp());
+
+
+// 引入url_launcher套件來啟動其他應用
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MainScreen(),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class MainScreen extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> images = [
+    'assets/page1.png',
+    'assets/page2.png',
+    'assets/page3.png',
+  ];
+  int currentPage = 0;
+  late PageController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(initialPage: currentPage);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var hiFlutter = const Text(
+      '',
+      style: TextStyle(fontSize: 20, color: Colors.white, fontStyle: FontStyle.italic),
+    );
+
     return Scaffold(
       body: Stack(
-        fit: StackFit.expand, // Ensure the stack fills the screen
         children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/main_background.png'),
-                fit: BoxFit.fill,
-              ),
+          PageView.builder(
+            controller: _controller,
+            itemCount: images.length,
+            onPageChanged: (int page) {
+              setState(() {
+                currentPage = page;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(images[index]),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Center(child: hiFlutter),
+              );
+            },
+          ),
+          Positioned(
+            left: 10,
+            bottom: 10,
+            child: Row(
+              children: List.generate(images.length, (index) {
+                return GestureDetector(
+                  onTap: () {
+                    _controller.animateToPage(index,
+                        duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 6),
+                    width: currentPage == index ? 25 : 15,  // 修改寬度
+                    height: 15,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: currentPage == index ? BoxShape.rectangle : BoxShape.circle,  // 修改形狀
+                      borderRadius: currentPage == index ? BorderRadius.circular(7.5) : null,  // 增加圓角
+                    ),
+                  ),
+                );
+              }),
             ),
           ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-                child: Text('Start'),
+          Positioned(
+            right: 10,
+            bottom: 10,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const Page2()));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              child: const Row(
+                children: [
+                  Text("Start"),
+                  Icon(Icons.arrow_forward_ios)
+                ],
               ),
             ),
           ),
